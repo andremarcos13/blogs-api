6d4dead2fabb65882cbaf49eddd726c8aa7dc453
Andre Marcos <andrmgc@gmail.com>
2022-09-26 16:39:26 -0300

    cria category model , req 7 e adiciona os outros arquivos de model

---
+++ src/models/Category.js
@@ -0,0 +1,17 @@
+const categoryModelSchema = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+          },
+          name: {
+            allowNull: false,
+            type: DataTypes.STRING,
+          }
+    });
+    return Category;
+}
+
+module.exports = categoryModelSchema;

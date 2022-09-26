46a66c9654f32b0641208fecd044ee1161a5732b
Andre Marcos <andrmgc@gmail.com>
2022-09-26 17:02:54 -0300

    model blogPost criado, req10 faltando apenas fazer as associacoes nas models

---
+++ src/models/BlogPost.js
@@ -0,0 +1,42 @@
+const blogPostModelSchema = (sequelize, DataTypes) => {
+    const BlogPost = sequelize.define('BlogPost', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+          },
+          title: {
+            allowNull: false,
+            type: DataTypes.STRING,        
+          },
+          content: {
+            allowNull: false,
+            type: DataTypes.STRING,
+          },
+          userId: {
+            allowNull: false,
+            type: DataTypes.INTEGER,
+            references:{
+              model: 'users',
+              key: 'id',
+            }
+          },
+          published: {
+            allowNull: false,
+            type: DataTypes.DATE,
+          },
+          updated: {
+            allowNull: false,
+            type: DataTypes.DATE,
+          },
+    },
+    {
+        tableName: 'blog_posts',
+        underscored: true,
+        timestamps: false,  
+    });
+    return BlogPost;
+}
+
+module.exports = blogPostModelSchema;

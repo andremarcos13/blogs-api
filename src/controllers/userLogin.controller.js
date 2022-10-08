a26dd4c58812296c4baf6447b1e5256fab14bf5d
Andre Marcos <andrmgc@gmail.com>
2022-10-08 20:51:10 -0300

    passando no req3

--- src/controllers/userLogin.controller.js
+++ src/controllers/userLogin.controller.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
 const UserService = require('../services/userLogin.service');
+
+const secret = process.env.JWT_SECRET || 'secretoken';
 
 const login = async (req, res) => {
     const { email, password } = req.body;
@@ -7,18 +10,21 @@ const login = async (req, res) => {
             message: 'Some required fields are missing',
           });
     }
-    // try {
         const users = await UserService.getByPasswordAndEmail(email, password);
+        // console.log('users controller', [users]);
         if (!users) {
             return res.status(400).json({
                 message: 'Invalid fields',
               });
         }
-        return res.status(200).json(users);
-    // } catch (e) {
-    //     console.log(e.message);
-    //     res.status(500).json({ message: 'error' });
-    // }
+        const jwtconfig = {
+            expiresIn: '1d',
+            algorithm: 'HS256',
+        };
+
+        const token = jwt.sign({ data: { userId: users.id } }, secret, jwtconfig);
+
+        return res.status(200).json({ token });
 };
 
 module.exports = {
